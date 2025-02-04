a4435898eb489397d70e64c247a7e9370de920c4
KarloSiric <ksiric99@gmail.com>
2025-02-04 23:05:41 +0100

    Adding new chapter for locations part of the game, adding location.h and location.c

---
+++ location.c
@@ -0,0 +1,6 @@
+/*
+* @Author: karlosiric
+* @Date:   2025-02-04 23:03:38
+* @Last Modified by:   karlosiric
+* @Last Modified time: 2025-02-04 23:03:53
+*/
---
+++ location.h
@@ -0,0 +1,4 @@
+// this is a linker module that is being used for main.c main program
+
+extern void executeLook(const char *noun);
+extern void executeGo(const char *noun);
